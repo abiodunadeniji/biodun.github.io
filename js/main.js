8586e84037bc298ce7fcd80970abec3e531dd37e
Abiodun adeniji <abiodunadeniji19@gmail.com>
2024-02-22 14:50:33 -0600

    edited core files

--- js/main.js
+++ js/main.js
@@ -115,6 +115,20 @@
 		});
 	};
 
+	var skillsWayPoint = function() {
+		if ($('#fh5co-skills').length > 0 ) {
+			$('#fh5co-skills').waypoint( function( direction ) {
+										
+				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+					setTimeout( pieChart , 400);					
+					$(this.element).addClass('animated');
+				}
+			} , { offset: '90%' } );
+		}
+
+	};
+
+
 	// Loading page
 	var loaderPage = function() {
 		$(".fh5co-loader").fadeOut("slow");
